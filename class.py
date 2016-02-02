67cf2ff7f236fef935a5bb296a7ca4d2cba54922
erickstm87 <erickstm@colorado.edu>
2016-02-02 12:47:40 -0700

    tried one initialization

--- class.py
+++ class.py
@@ -6,11 +6,12 @@ class Product:
     def __init__(self,price,p_id,quantity):
         self.price = price
         self.p_id = p_id
-        self.quantity = quantity)
+        self.quantity = quantity
    
-class Inventory:
+#class Inventory:
 
-     
+gramophone = Product(20,'instrument',13)     
+print gramophone.price
 
 
 
