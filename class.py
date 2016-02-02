d7bc927b4c846ebcbf09b79a0c11b1d539d4ca63
erickstm87 <erickstm@colorado.edu>
2016-02-02 12:09:12 -0700

    started on the class practice

---
+++ class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+#Author Tommy Erickson
+#Class writing practice taken from https://github.com/karan/Projects#classes
+
+class Product:
+    def __init__(self,price,p_id,quantity):
+        self.price = price
+        self.p_id = p_id
+        self.quantity = quantity
