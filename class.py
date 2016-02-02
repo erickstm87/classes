16a112d610edfa639429e67acc9b647d27d2287d
erickstm87 <erickstm@colorado.edu>
2016-02-02 12:26:02 -0700

    added some notes on init vs the class in general

--- class.py
+++ class.py
@@ -3,7 +3,9 @@
 #Class writing practice taken from https://github.com/karan/Projects#classes
 
 class Product:
+    #general note: init is called because it will call an instance of the object. An object can have ''class attributes'' which are outside of the init function and apply to all instances of the object and an object can have 'instance attributes' which are things that need to be assigned every time the instance is called
     def __init__(self,price,p_id,quantity):
         self.price = price
         self.p_id = p_id
         self.quantity = quantity
+
