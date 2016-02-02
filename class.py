7e8b322a75ece143d988c96c72504d5e8d4e2e1c
erickstm87 <erickstm@colorado.edu>
2016-02-02 12:38:54 -0700

    trying to reset

--- class.py
+++ class.py
@@ -4,8 +4,11 @@
 
 class Product:
     #general note: init is called because it will call an instance of the object. An object can have ''class attributes'' which are outside of the init function and apply to all instances of the object and an object can have 'instance attributes' which are things that need to be assigned every time the instance is called
-    def __init__(self,price,p_id,quantity):
-        self.price = price
-        self.p_id = p_id
-        self.quantity = quantity
+   self.price = price
+   p_id = 'gramophone'
+   quantity = 50
+     
+class Inventory:
+    
+    
 
